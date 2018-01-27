53ca8417713f01b4694efac7fb2c241203d8d88f
ccchenhen <31816586+ccchenhen@users.noreply.github.com>
2018-01-27 14:07:35 +0800

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -43,34 +43,36 @@ class Server:
 
     def login(self, sock):
         #read the msg that should have login code plus username
-        msg = myrecv(sock)
-        if len(msg) > 0:
-            code = msg[0]
+        try:
+            msg = json.loads(myrecv(sock))
+            if len(msg) > 0:
 
-            if code == M_LOGIN:
-                name = msg[1:]
-                if self.group.is_member(name) != True:
-                    #move socket from new clients list to logged clients
-                    self.new_clients.remove(sock)
-                    #add into the name to sock mapping
-                    self.logged_name2sock[name] = sock
-                    self.logged_sock2name[sock] = name
-                    #load chat history of that user
-                    if name not in self.indices.keys():
-                        try:
-                            self.indices[name]=pkl.load(open(name+'.idx','rb'))
-                        except IOError: #chat index does not exist, then create one
-                            self.indices[name] = indexer.Index(name)
-                    print(name + ' logged in')
-                    self.group.join(name)
-                    mysend(sock, M_LOGIN + 'ok')
-                else: #a client under this name has already logged in
-                    mysend(sock, M_LOGIN + 'duplicate')
-                    print(name + ' duplicate login attempt')
-            else:
-                print ('wrong code received')
-        else: #client died unexpectedly
-            self.logout(sock)
+                if msg["action"] == "login":
+                    name = msg["name"]
+                    if self.group.is_member(name) != True:
+                        #move socket from new clients list to logged clients
+                        self.new_clients.remove(sock)
+                        #add into the name to sock mapping
+                        self.logged_name2sock[name] = sock
+                        self.logged_sock2name[sock] = name
+                        #load chat history of that user
+                        if name not in self.indices.keys():
+                            try:
+                                self.indices[name]=pkl.load(open(name+'.idx','rb'))
+                            except IOError: #chat index does not exist, then create one
+                                self.indices[name] = indexer.Index(name)
+                        print(name + ' logged in')
+                        self.group.join(name)
+                        mysend(sock, json.dumps({"action":"login", "status":"ok"}))
+                    else: #a client under this name has already logged in
+                        mysend(sock, json.dumps({"action":"login", "status":"duplicate"}))
+                        print(name + ' duplicate login attempt')
+                else:
+                    print ('wrong code received')
+            else: #client died unexpectedly
+                self.logout(sock)
+        except:
+            self.all_sockets.remove(sock)
 
     def logout(self, sock):
         #remove sock from all lists
@@ -90,77 +92,77 @@ class Server:
         #read msg code
         msg = myrecv(from_sock)
         if len(msg) > 0:
-            code = msg[0]
 #==============================================================================
 # handle connect request
 #==============================================================================
-            if code == M_CONNECT:
-                to_name = msg[1:]
+            msg = json.loads(msg)
+            if msg["action"] == "connect":
+                to_name = msg["target"]
                 from_name = self.logged_sock2name[from_sock]
                 if to_name == from_name:
-                    msg = M_CONNECT + 'hey you'
+                    msg = json.dumps({"action":"connect", "status":"self"})
                 # connect to the peer
                 elif self.group.is_member(to_name):
                     to_sock = self.logged_name2sock[to_name]
                     self.group.connect(from_name, to_name)
                     the_guys = self.group.list_me(from_name)
-                    msg = M_CONNECT + 'ok'
+                    msg = json.dumps({"action":"connect", "status":"success"})
                     for g in the_guys[1:]:
                         to_sock = self.logged_name2sock[g]
-                        mysend(to_sock, M_CONNECT + from_name)
+                        mysend(to_sock, json.dumps({"action":"connect", "status":"request", "from":from_name}))
                 else:
-                    msg = M_CONNECT + 'no_user'
+                    msg = json.dumps({"action":"connect", "status":"no-user"})
                 mysend(from_sock, msg)
 #==============================================================================
 # handle messeage exchange: one peer for now. will need multicast later
 #==============================================================================
-            elif code == M_EXCHANGE:
+            elif msg["action"] == "exchange":
                 from_name = self.logged_sock2name[from_sock]
                 the_guys = self.group.list_me(from_name)
-                said = msg[1:]
-                said2 = text_proc(said, from_name)
+                #said = msg["from"]+msg["message"]
+                said2 = text_proc(msg["message"], from_name)
                 self.indices[from_name].add_msg_and_index(said2)
                 for g in the_guys[1:]:
                     to_sock = self.logged_name2sock[g]
-                    self.indices[g].add_msg_and_index(said2)
-                    mysend(to_sock, msg)
+                    self.indices[g].add_msg_and_index(said2)                
+                    mysend(to_sock, json.dumps({"action":"exchange", "from":msg["from"], "message":msg["message"]}))
 #==============================================================================
 #                 listing available peers
 #==============================================================================
-            elif code == M_LIST:
+            elif msg["action"] == "list":
                 from_name = self.logged_sock2name[from_sock]
                 msg = self.group.list_all(from_name)
-                mysend(from_sock, msg)
+                mysend(from_sock, json.dumps({"action":"list", "results":msg}))
 #==============================================================================
 #             retrieve a sonnet
 #==============================================================================
-            elif code == M_POEM:
-                poem_indx = int(msg[1:])
+            elif msg["action"] == "poem":
+                poem_indx = int(msg["target"])
                 from_name = self.logged_sock2name[from_sock]
                 print(from_name + ' asks for ', poem_indx)
                 poem = self.sonnet.get_sect(poem_indx)
                 print('here:\n', poem)
-                mysend(from_sock, M_POEM + poem)
+                mysend(from_sock, json.dumps({"action":"poem", "results":poem}))
 #==============================================================================
 #                 time
 #==============================================================================
-            elif code == M_TIME:
+            elif msg["action"] == "time":
                 ctime = time.strftime('%d.%m.%y,%H:%M', time.localtime())
-                mysend(from_sock, ctime)
+                mysend(from_sock, json.dumps({"action":"time", "results":ctime}))
 #==============================================================================
 #                 search
 #==============================================================================
-            elif code == M_SEARCH:
-                term = msg[1:]
+            elif msg["action"] == "search":
+                term = msg["target"]
                 from_name = self.logged_sock2name[from_sock]
                 print('search for ' + from_name + ' for ' + term)
                 search_rslt = (self.indices[from_name].search(term)).strip()
                 print('server side search: ' + search_rslt)
-                mysend(from_sock, M_SEARCH + search_rslt)
+                mysend(from_sock, json.dumps({"action":"search", "results":search_rslt}))
 #==============================================================================
 # the "from" guy has had enough (talking to "to")!
 #==============================================================================
-            elif code == M_DISCONNECT:
+            elif msg["action"] == "disconnect":
                 from_name = self.logged_sock2name[from_sock]
                 the_guys = self.group.list_me(from_name)
                 self.group.disconnect(from_name)
@@ -168,12 +170,11 @@ class Server:
                 if len(the_guys) == 1:  # only one left
                     g = the_guys.pop()
                     to_sock = self.logged_name2sock[g]
-                    mysend(to_sock, M_DISCONNECT)
+                    mysend(to_sock, json.dumps({"action":"disconnect"}))
 #==============================================================================
 #                 the "from" guy really, really has had enough
 #==============================================================================
-            elif code == M_LOGOUT:
-                self.logout(from_sock)
+
         else:
             #client died unexpectedly
             self.logout(from_sock)
