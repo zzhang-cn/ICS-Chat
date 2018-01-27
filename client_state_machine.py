cb045596a110b009243ccbf58f52279d5f03ee01
ccchenhen <31816586+ccchenhen@users.noreply.github.com>
2018-01-27 14:02:45 +0800

    Update client_state_machine.py

--- client_state_machine.py
+++ client_state_machine.py
@@ -27,28 +27,28 @@ class ClientSM:
         return self.me
         
     def connect_to(self, peer):
-        msg = M_CONNECT + peer
+        msg = json.dumps({"action":"connect", "target":peer})
         mysend(self.s, msg)
-        response = myrecv(self.s)
-        if response == (M_CONNECT+'ok'):
+        response = json.loads(myrecv(self.s))
+        if response["status"] == "success":
             self.peer = peer
             self.out_msg += 'You are connected with '+ self.peer + '\n'
             return (True)
-        elif response == (M_CONNECT + 'busy'):
+        elif response["status"] == "busy":
             self.out_msg += 'User is busy. Please try again later\n'
-        elif response == (M_CONNECT + 'hey you'):
+        elif response["status"] == "self":
             self.out_msg += 'Cannot talk to yourself (sick)\n'
         else:
             self.out_msg += 'User is not online, try again later\n'
         return(False)
 
     def disconnect(self):
-        msg = M_DISCONNECT
+        msg = json.dumps({"action":"disconnect"})
         mysend(self.s, msg)
         self.out_msg += 'You are disconnected from ' + self.peer + '\n'
         self.peer = ''
 
-    def proc(self, my_msg, peer_code, peer_msg):
+    def proc(self, my_msg, peer_msg):
         self.out_msg = ''
 #==============================================================================
 # Once logged in, do a few things: get peer listing, connect, search
@@ -64,13 +64,13 @@ class ClientSM:
                     self.state = S_OFFLINE
                     
                 elif my_msg == 'time':
-                    mysend(self.s, M_TIME)
-                    time_in = myrecv(self.s)
+                    mysend(self.s, json.dumps({"action":"time"}))
+                    time_in = json.loads(myrecv(self.s))["results"]
                     self.out_msg += "Time is: " + time_in
                             
                 elif my_msg == 'who':
-                    mysend(self.s, M_LIST)
-                    logged_in = myrecv(self.s)
+                    mysend(self.s, json.dumps({"action":"list"}))
+                    logged_in = json.loads(myrecv(self.s))["results"]
                     self.out_msg += 'Here are all the users in the system:\n'
                     self.out_msg += logged_in
                             
@@ -86,8 +86,8 @@ class ClientSM:
                         
                 elif my_msg[0] == '?':
                     term = my_msg[1:].strip()
-                    mysend(self.s, M_SEARCH + term)
-                    search_rslt = myrecv(self.s)[1:].strip()
+                    mysend(self.s, json.dumps({"action":"search", "target":term}))
+                    search_rslt = json.loads(myrecv(self.s))["results"][1:].strip()
                     if (len(search_rslt)) > 0:
                         self.out_msg += search_rslt + '\n\n'
                     else:
@@ -95,27 +95,20 @@ class ClientSM:
                         
                 elif my_msg[0] == 'p':
                     poem_idx = my_msg[1:].strip()
-                    mysend(self.s, M_POEM + poem_idx)
-                    poem = myrecv(self.s)[1:].strip()
+                    mysend(self.s, json.dumps({"action":"poem", "target":poem_idx}))
+                    poem = json.loads(myrecv(self.s))["results"][1:].strip()
                     if (len(poem) > 0):
                         self.out_msg += poem + '\n\n'
                     else:
                         self.out_msg += 'Sonnet ' + poem_idx + ' not found\n\n'
 
-                elif my_msg[0] == '?':
-                    term = my_msg[1:].strip()
-                    mysend(self.s, M_SEARCH + term)
-                    search_rslt = myrecv(self.s)[1:].strip()
-                    if (len(search_rslt)) > 0:
-                        self.out_msg += search_rslt + '\n\n'
-                    else:
-                        self.out_msg += '\'' + term + '\'' + ' not found\n\n'
                 else:
                     self.out_msg += menu
                     
             if len(peer_msg) > 0:
-                if peer_code == M_CONNECT:
-                    self.peer = peer_msg
+                peer_msg = json.loads(peer_msg)
+                if peer_msg["action"] == "connect":
+                    self.peer = peer_msg["from"]
                     self.out_msg += 'Request from ' + self.peer + '\n'
                     self.out_msg += 'You are connected with ' + self.peer 
                     self.out_msg += '. Chat away!\n\n'
@@ -128,20 +121,20 @@ class ClientSM:
 #==============================================================================
         elif self.state == S_CHATTING:
             if len(my_msg) > 0:     # my stuff going out
-                mysend(self.s, M_EXCHANGE + "[" + self.me + "] " + my_msg)
+                mysend(self.s, json.dumps({"action":"exchange", "from":"[" + self.me + "]", "message":my_msg}))
                 if my_msg == 'bye':
                     self.disconnect()
                     self.state = S_LOGGEDIN
                     self.peer = ''
             if len(peer_msg) > 0:    # peer's stuff, coming in
-                if peer_code == M_CONNECT:
-                    self.out_msg += "(" + peer_msg + " joined)\n"
+                peer_msg = json.loads(peer_msg)
+                if peer_msg["action"] == "connect":
+                    self.out_msg += "(" + peer_msg["from"] + " joined)\n"
+                elif peer_msg["action"] == "disconnect":
+                    self.state = S_LOGGEDIN
                 else:
-                    self.out_msg += peer_msg
+                    self.out_msg += peer_msg["from"] + peer_msg["message"]
 
-            # I got bumped out
-            if peer_code == M_DISCONNECT:
-                self.state = S_LOGGEDIN
 
             # Display the menu again
             if self.state == S_LOGGEDIN:
